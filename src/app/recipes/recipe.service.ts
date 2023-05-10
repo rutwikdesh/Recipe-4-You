c0fcb62a90a18c7bc1c97791eb5dc7f5e6783350
Rutwik Deshmukh <rutwikdesh28@gmail.com>
2023-05-10 20:15:49 +0530

    added recipe details routing

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -28,6 +28,10 @@ export class RecipeService {
     return this.recipes.slice();
   }
 
+  getRecipe(id: number) {
+    return this.recipes[id];
+  }
+
   addIngredientsToShoppingList(ingredientsAdded: Ingredient[]) {
     this.shoppingListService.addIngredientsToShoppingList(ingredientsAdded);
   }
